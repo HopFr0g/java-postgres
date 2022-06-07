313f9b4a3d32fab9c5e5c7ada7ad5af53543783d
Leonel Poletti <poletti.leonel@gmail.com>
2022-06-07 19:58:11 -0300

    Front end estilos mejorados (diseño responsive)

--- src/main/resources/static/scripts/responsive.js
+++ src/main/resources/static/scripts/responsive.js
@@ -1,6 +1,22 @@
 const responsiveMenu = document.querySelector(".nav-responsive__a-container");
 const menuButton = document.querySelector(".nav-responsive__button");
+const menuBlur = document.querySelector(".main__blur");
 
 menuButton.addEventListener("click", () => {
-    responsiveMenu.style.transform == "" ? responsiveMenu.style.transform = "TranslateY(100vh)" : responsiveMenu.style.transform = "";
+    responsiveMenu.style.transform == "" ? displayMenu() : hideMenu();
 });
+
+menuBlur.addEventListener("click", () => {
+    if (menuBlur.style.opacity == "1")
+        hideMenu();
+});
+
+const displayMenu = () => {
+    responsiveMenu.style.transform = "TranslateY(100vh)";
+    menuBlur.style.opacity = "1";
+}
+
+const hideMenu = () => {
+    responsiveMenu.style.transform = "";
+    menuBlur.style.opacity = "0";
+}
